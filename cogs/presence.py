cf541ed65705233410091e2cf8dca63b466a941d
ActuallyFlamey <flamey@hexa.blue>
2026-09-07 23:05:23 +0200

    add: new presences

--- cogs/presence.py
+++ cogs/presence.py
@@ -3,6 +3,8 @@ import itertools
 import discord
 from discord.ext import commands, tasks
 
+from schema import Egg
+
 
 class Presence(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -17,8 +19,13 @@ class Presence(commands.Cog):
             "Donate at ko-fi.com/hexablue",
             "Surprisingly not about DELTARUNE.",
             "I like my Eggs open-source.",
+            "Well, there is a bot here.",
             "Support at discord.gg/G9vfEZGZnT",
-            "Gambling without any stakes! So awesome!"
+            "Delivering {eggs} Eggs!",
+            "Gambling without any stakes! So awesome!",
+            "/challenge your friends to an Egg battle!",
+            "Well, there was not a bot here.",
+            "It's like Easter, but all year round!",
         ])
 
         self.update_presence.start()
@@ -27,7 +34,14 @@ class Presence(commands.Cog):
     async def update_presence(self):
         presence = next(self.activities)
 
-        await self.bot.change_presence(activity=discord.CustomActivity(name=presence.format(guilds=len(self.bot.guilds))))
+        await self.bot.change_presence(
+            activity=discord.CustomActivity(
+                name=presence.format(
+                    guilds=len(self.bot.guilds),
+                    eggs=await Egg.all().count()
+                )
+            )
+        )
 
     @update_presence.before_loop
     async def before_update_presence(self):
